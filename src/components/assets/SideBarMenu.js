98cc21fd765f38a91222f030abe8a569c886c590
DenisOuma <dennisouma38@gmail.com>
2022-12-10 09:25:24 +0300

    Adding material ui icons and styles and implimenting display functionality to the app

--- src/components/assets/SideBarMenu.js
+++ src/components/assets/SideBarMenu.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "../../styles/NavBar.css";
 import { Icon } from "@iconify/react";
-
+import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 function SideBarMenu() {
 	const [isShown, setIsShown] = React.useState(false);
+	const [dropDown, setDropDown] = React.useState(false);
 
 	const handleClick = (e) => {
 		setIsShown((current) => !current);
+	};
+
+	const handleOpen = (e) => {
+		setDropDown((current) => !current);
 	};
 	return (
 		<div className="tagle-open">
@@ -30,6 +35,33 @@ function SideBarMenu() {
 			{isShown && (
 				<div className="navigation-menue">
 					<h2>EXPLORER</h2>
+					<div className="drop-open-menu">
+						<KeyboardArrowRightRoundedIcon />
+						<h3 onClick={handleOpen}>About</h3>
+					</div>
+					{dropDown && (
+						<>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-html" color="gray" />
+								<p>index.html</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-css" color="gray" />
+								<p>experience.css</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-js-official" color="gray" />
+								<p>skills.js</p>
+							</div>
+							<div className="options-menu">
+								<Icon
+									icon="vscode-icons:file-type-typescript-official"
+									color="gray"
+								/>
+								<p>projects.ts</p>
+							</div>
+						</>
+					)}
 				</div>
 			)}
 		</div>
