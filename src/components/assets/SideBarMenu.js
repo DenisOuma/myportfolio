d371b40b0e7b0c7156fef156c9492b18086e86ce
DenisOuma <dennisouma38@gmail.com>
2022-12-10 08:53:39 +0300

    Adding sidebar on button click and hide

--- src/components/assets/SideBarMenu.js
+++ src/components/assets/SideBarMenu.js
@@ -3,18 +3,35 @@ import "../../styles/NavBar.css";
 import { Icon } from "@iconify/react";
 
 function SideBarMenu() {
+	const [isShown, setIsShown] = React.useState(false);
+
+	const handleClick = (e) => {
+		setIsShown((current) => !current);
+	};
 	return (
-		<div className="side-bar-meny">
-			<div className="hover-effect">
-				<Icon className="icon-side-bar" icon="fluent-mdl2:copy" color="white" />
+		<div className="tagle-open">
+			<div className="side-bar-meny">
+				<div className="hover-effect">
+					<Icon
+						onClick={handleClick}
+						className="icon-side-bar"
+						icon="fluent-mdl2:copy"
+						color="white"
+					/>
+				</div>
+				<div className="position-bottom hover-effect">
+					<Icon
+						className="icon-side-bar"
+						icon="iconoir:profile-circled"
+						color="gray"
+					/>
+				</div>
 			</div>
-			<div className="position-bottom hover-effect">
-				<Icon
-					className="icon-side-bar"
-					icon="iconoir:profile-circled"
-					color="gray"
-				/>
-			</div>
+			{isShown && (
+				<div className="navigation-menue">
+					<h2>EXPLORER</h2>
+				</div>
+			)}
 		</div>
 	);
 }
