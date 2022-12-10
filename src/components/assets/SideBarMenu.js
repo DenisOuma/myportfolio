d33bf987a1feca4ac70c78cfef04ac04013261a0
DenisOuma <dennisouma38@gmail.com>
2022-12-10 09:54:15 +0300

    Sidebar section completed

--- src/components/assets/SideBarMenu.js
+++ src/components/assets/SideBarMenu.js
@@ -4,14 +4,26 @@ import { Icon } from "@iconify/react";
 import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 function SideBarMenu() {
 	const [isShown, setIsShown] = React.useState(false);
-	const [dropDown, setDropDown] = React.useState(false);
+	const [about, setAbout] = React.useState(false);
+	const [project, setProject] = React.useState(false);
+	const [contact, setContact] = React.useState(false);
+	const [hobbies, setHobbies] = React.useState(false);
 
 	const handleClick = (e) => {
 		setIsShown((current) => !current);
 	};
 
-	const handleOpen = (e) => {
-		setDropDown((current) => !current);
+	const handleAbout = (e) => {
+		setAbout((current) => !current);
+	};
+	const handleProject = (e) => {
+		setProject((current) => !current);
+	};
+	const handleContact = (e) => {
+		setContact((current) => !current);
+	};
+	const handleHobbies = (e) => {
+		setHobbies((current) => !current);
 	};
 	return (
 		<div className="tagle-open">
@@ -37,9 +49,9 @@ function SideBarMenu() {
 					<h2>EXPLORER</h2>
 					<div className="drop-open-menu">
 						<KeyboardArrowRightRoundedIcon />
-						<h3 onClick={handleOpen}>About</h3>
+						<h3 onClick={handleAbout}>About</h3>
 					</div>
-					{dropDown && (
+					{about && (
 						<>
 							<div className="options-menu">
 								<Icon icon="vscode-icons:file-type-html" color="gray" />
@@ -59,6 +71,69 @@ function SideBarMenu() {
 									color="gray"
 								/>
 								<p>projects.ts</p>
+							</div>
+						</>
+					)}
+					<div className="drop-open-menu">
+						<KeyboardArrowRightRoundedIcon />
+						<h3 onClick={handleProject}>Projects</h3>
+					</div>
+					{project && (
+						<>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-reactjs" color="gray" />
+								<p>quickMart.js</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="logos:nodejs-icon" color="gray" />
+								<p>duka.js</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-ruby" color="gray" />
+								<p>contact_keeper.rb</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-reactjs" color="gray" />
+								<p>Expense.jsx</p>
+							</div>
+						</>
+					)}
+					<div className="drop-open-menu">
+						<KeyboardArrowRightRoundedIcon />
+						<h3 onClick={handleContact}>Contact</h3>
+					</div>
+					{contact && (
+						<>
+							<div className="options-menu">
+								<Icon icon="logos:google-gmail" color="gray" width="14" />
+								<p>Email.txt</p>
+							</div>
+						</>
+					)}
+					<div className="drop-open-menu">
+						<KeyboardArrowRightRoundedIcon />
+						<h3 onClick={handleHobbies}>Misc</h3>
+					</div>
+					{hobbies && (
+						<>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-python" color="gray" />
+								<p>Hobbies.py</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-css" color="gray" />
+								<p>Blogs.css</p>
+							</div>
+							<div className="options-menu">
+								<Icon icon="vscode-icons:file-type-js-official" color="gray" />
+								<p>Learning.js</p>
+							</div>
+							<div className="options-menu">
+								<Icon
+									icon="vscode-icons:file-type-typescript-official"
+									color="gray"
+								/>
+								<p>Gaming.ts</p>
 							</div>
 						</>
 					)}
