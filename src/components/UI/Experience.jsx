74429dee580f9410f8eafe9df7751da8122a9529
DenisOuma <dennisouma38@gmail.com>
2022-12-12 14:21:29 +0300

    Website design working on the Industrial experience

--- src/components/UI/Experience.jsx
+++ src/components/UI/Experience.jsx
@@ -7,7 +7,31 @@ function Experience() {
 				<h1>Work</h1>
 				<h2>Industry Experience</h2>
 				<div className="thread-section">
-					<div className="experienc-1"></div>
+					<div className="experienc-1">
+						<div className="mohi">
+							<div className="responsibility">
+								<p>
+									Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+									Cupiditate aperiam eveniet quam, repudiandae ipsa nulla.
+								</p>
+							</div>
+							<div
+								className="img-icon"
+								style={{
+									height: "60px",
+									width: "60px",
+									backgroundColor: "red",
+									borderRadius: "50%",
+									border: "2px solid #fff",
+								}}
+							>
+								<img
+									src="https://cdn2.hubspot.net/hubfs/4458293/Imported_Blog_Media/MOHI_Icon_small.jpg"
+									alt="MOHI"
+								/>
+							</div>
+						</div>
+					</div>
 					<div className="empty-div"></div>
 					<div className="experience-2"></div>
 				</div>
